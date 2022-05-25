0993fb4a992fef15d5e418ee808a1a33c3466e40
bombzj <bombzj@gmail.com>
2022-05-25 16:17:26 +0800

    split file

---
+++ pvz2util.js
@@ -0,0 +1,248 @@
+
+// 画一个植物
+function plant(i, x, y, act) {
+    let pam = pams[plantList[i].pamName]
+    let a
+    if(pam.spriteMap[act]) {
+        a = new PamSprite(pam, pam.spriteMap[act])
+    } else {
+        a = new PamSprite(pam, pam.main_sprite, pam.actionFrame[act])
+    }
+    a.position.set(x, y)
+    app.stage.addChild(a)
+    a.plantType = i
+    a.scale.set(resScale)
+    return a
+}
+
+// 画一个种子
+function seed(i, x, y) {
+    let textures = loader.resources.UI_SeedPackets_768_00.textures
+    let c = new PIXI.Container()
+    
+    let bgname = plantList[i].world
+    if(!bgname || bgname == 'tutorial') bgname = 'ready'
+    let b = new PIXI.Sprite(textures['IMAGE_UI_PACKETS_' + bgname.toUpperCase()])
+    // b.position.set(x, y)
+    let priceTab = new PIXI.Sprite(textures.IMAGE_UI_PACKETS_PRICE_TAB)
+    priceTab.position.set(70, 35)
+
+    let price = new PIXI.Text(plantList[i].cost ,{fontFamily : 'Arial', fontSize: 32, fill : 'white', align : 'center', fontWeight:'600', strokeThickness:3});
+    price.position.set(115 - price.width, 40)
+
+    let a = new PIXI.Sprite(textures['IMAGE_UI_PACKETS_' + plantList[i].ename.toUpperCase()])
+    a.position.set(10, 0)
+    a.seedType = i
+
+    c.addChild(b, a, priceTab, price)
+    c.position.set(x, y)
+    app.stage.addChild(c)
+    return c
+}
+
+// 画背景
+function back(x, y) {
+    let textures = loader.resources.DelayLoad_Background_FrontLawn_Birthday_768_00.textures
+    // let a = new PIXI.Sprite(textures.IMAGE_BACKGROUNDS_BACKGROUND_LOD_BIRTHDAY_TEXTURE_LEFT)
+    // a.position.set(x, y)
+    let b = new PIXI.Sprite(textures.IMAGE_BACKGROUNDS_BACKGROUND_LOD_BIRTHDAY_TEXTURE)
+    b.position.set(x, y)
+    app.stage.addChild(b)
+    return b
+}
+
+// 画太阳
+function sun(x, y) {
+    let pam = pams.SUN
+    let a = new PamSprite(pam, pam.main_sprite)
+    a.position.set(x, y)
+    app.stage.addChild(a)
+    a.scale.set(resScale)
+    return a
+}
+
+// 画太阳商量
+function numSun(x, y, num = 0) {
+    let c = new PIXI.Container()
+    let textures = loader.resources.UI_ALWAYSLOADED_768_00.textures
+    let b = new PIXI.Sprite(textures.IMAGE_UI_GENERIC_COUNTER_BG)
+    b.position.set(35, 15)
+    b.scale.x = 1.2
+    b.alpha = 0.7
+    let a = new PIXI.Sprite(textures.IMAGE_UI_HUD_INGAME_SUN)
+
+    let cnt = new PIXI.Text(num ,{fontFamily : 'Arial', fontSize: 32, fill : 'white', align : 'center', fontWeight:'600', strokeThickness:3});
+    cnt.position.set(75, 12)
+
+    c.addChild(b, a, cnt)
+    c.position.set(x, y)
+    app.stage.addChild(c)
+    return c
+}
+
+// 画小车
+function car(x, y, act) {
+    let pam = pams.MOWER_MODERN
+    let a = new PamSprite(pam, pam.main_sprite, pam.actionFrame[act])
+    a.position.set(x, y)
+    app.stage.addChild(a)
+    a.scale.set(resScale)
+    return a
+}
+
+
+const resScale = 768 / 1200
+
+const worldBg = {
+    egypt: 1,
+    pirate: 2,
+}
+
+
+
+function setup(resources, callback) {
+    for(let p of pamList) {
+        for(let name of p.name) {
+            if(resources[name].data) {
+                let textures = {}
+                if(typeof p.image == 'string') {
+                    textures = resources[p.image].textures
+                } else {
+                    for(let i of p.image) {
+                        Object.assign(textures, resources[i].textures)
+                    }
+                }
+                pamInit(name, resources[name].data, textures)
+            }
+        }
+    }
+    plantType = resources.planttype.data
+    for(let p of plantList) {
+        let t = plantType[p.ename]
+        Object.assign(p, t)
+    }
+    init(resources)
+    app.ticker.add(delta => loop())
+}
+
+
+const pamList = [
+    {
+        name: ['SUNFLOWER'],
+        image: 'PlantSunflower_768_00'
+    },
+    {
+        name: ['PEASHOOTER'],
+        image: 'PLANTPEASHOOTER_768_00'
+    },
+    {
+        name: ['SNOWPEA', 'SNOWPEA_PLANTFOOD', 'T_SNOW_PEA'],
+        image: 'PLANTSNOWPEA_768_00'
+    },
+    {
+        name: ['CHERRYBOMB', 'CHERRYBOMB_EXPLOSION_TOP', 'CHERRYBOMB_EXPLOSION_REAR'],
+        image: 'PLANTCHERRYBOMB_768_00'
+    },
+    {
+        name: ['SUNFLOWER_TWIN'],
+        image: 'PLANTTWINSUNFLOWER_768_00'
+    },
+    {
+        name: ['THREEPEATER'],
+        image: 'PLANTTHREEPEATER_768_00'
+    },
+    {
+        name: ['SQUASH'],
+        image: 'PLANTSQUASH_768_00'
+    },
+    {
+        name: ['ZOMBIE_MODERN_ALLSTAR'],
+        image: 'ZOMBIEMODERNALLSTARGROUP_768_00'
+    },
+    {
+        name: ['ZOMBIE_EGYPT_BASIC'],
+        image: 'ZombieEgyptBasicGroup_768_00'
+    },
+    {
+        name: ['COCONUTCANNON', 'T_COCONUT_PROJECTILE_EXPLOSION'],
+        image: 'PlantCoconutCannon_768_00'
+    },
+    {
+        name: ['FUMESHROOM', 'FUMESHROOM_BUBBLES', 'FUMESHROOM_BUBBLES_HIT'],
+        image: 'PLANTFUMESHROOM_768_00'
+    },
+    {
+        name: ['BLOOMERANG', 'T_BLOOMERANG_PROJECTILE'],
+        image: 'PlantBloomerang_768_00'
+    },
+    {
+        name: ['BONKCHOY'],
+        image: 'PLANTBONKCHOY_768_00'
+    },
+    {
+        name: ['WALLNUT'],
+        image: 'PLANTWALLNUT_768_00'
+    },
+    {
+        name: ['CHOMPER'],
+        image: 'PLANTCHOMPER_768_00'
+    },
+    {
+        name: ['REPEATER'],
+        image: 'PLANTREPEATER_768_00'
+    },
+    {
+        name: ['GARGANTUAR'],
+        image: 'ZOMBIEFUTUREGARGANTUARGROUP_768_00'
+    },
+    {
+        name: ['SNAPDRAGON', 'SNAPDRAGON_FIRE'],
+        image: 'PlantSnapdragon_768_00'
+    },
+    {
+        name: [],
+        image: ['DelayLoad_Background_FrontLawn_768_00', 'Grass_Transition_768_00', 'UI_SeedPackets_768_00', 'DelayLoad_Background_FrontLawn_Birthday_768_00']
+    },
+    {
+        name: [],
+        image: ['UI_ALWAYSLOADED_768_00']
+    },
+    {
+        name: ['T_PEA_PROJECTILE'],
+        image: 'PEAEFFECTS_768_00'
+    },
+    {
+        name: ['ZOMBIE_ASH', 'T_SPLAT_SNOW_PEA', 'SUN'],
+        image: 'levelcommon_768_00'
+    },
+    {
+        name: ['SODROLL'],
+        image: 'SodRollGroup_768_00'
+    },
+    {
+        name: ['MOWER_MODERN'],
+        image: 'MODERNMOWERGROUP_768_00'
+    }
+]
+
+const loadJsons = [/*'planttypes'*/]
+var plantType
+function loadPams(callback) {
+    for(let p of pamList) {
+        for(let name of p.name) {
+            loader.add(name, "pam/pams/" + name + ".json")
+        }
+        if(typeof p.image == 'string') {
+            loader.add(p.image, "pam/json/" + p.image + ".json")
+        } else {
+            for(let i of p.image) {
+                loader.add(i, "pam/json/" + i + ".json")
+            }
+        }
+    }
+    for(let j of loadJsons) {
+        loader.add(j, 'packages/' + j + '.rton.json')
+    }
+    loader.add('planttype', 'pam/planttype.json')
+    loader.load((loader, resources) => setup(resources, callback));
+}
